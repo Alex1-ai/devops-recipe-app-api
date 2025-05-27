3360bc153c8c8a7b78d4ba0b5611e283dcff6fd5
Alex1.ai <alexanderemmanuel1719@gmail.com>
2025-05-27 15:18:55 +0100

    made changes to the iam.tf

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -1,37 +1,29 @@
-################################################################################
-# Create IAM user and policies for Continuous Deploy (CD) account #
-################################################################################
-
+#######################################################################
+# Create IAM user and policies for Continuous Deployment (CD) account #
+#######################################################################
 resource "aws_iam_user" "cd" {
   name = "recipe-app-api-cd"
 }
-
 resource "aws_iam_access_key" "cd" {
   user = aws_iam_user.cd.name
 }
-
-
-################################################################################
-# Create for Terraform backend to S3 and Dynamo DB access #
-################################################################################
+#########################################################
+# Policy for Teraform backend to S3 and DynamoDB access #
+#########################################################
 data "aws_iam_policy_document" "tf_backend" {
   statement {
     effect    = "Allow"
     actions   = ["s3:ListBucket"]
     resources = ["arn:aws:s3:::${var.tf_state_bucket}"]
   }
-
-
   statement {
     effect  = "Allow"
     actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
     resources = [
       "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy/*",
       "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy-env/*"
-
     ]
   }
-
   statement {
     effect = "Allow"
     actions = [
@@ -40,38 +32,27 @@ data "aws_iam_policy_document" "tf_backend" {
       "dynamodb:PutItem",
       "dynamodb:DeleteItem"
     ]
-
     resources = ["arn:aws:dynamodb:*:*:table/${var.tf_state_lock_table}"]
-
-
   }
 }
-
-
 resource "aws_iam_policy" "tf_backend" {
   name        = "${aws_iam_user.cd.name}-tf-s3-dynamodb"
   description = "Allow user to use S3 and DynamoDB for TF backend resources"
   policy      = data.aws_iam_policy_document.tf_backend.json
 }
-
-
 resource "aws_iam_user_policy_attachment" "tf_backend" {
   user       = aws_iam_user.cd.name
   policy_arn = aws_iam_policy.tf_backend.arn
 }
-
-
 #########################
 # Policy for ECR access #
 #########################
-
 data "aws_iam_policy_document" "ecr" {
   statement {
     effect    = "Allow"
     actions   = ["ecr:GetAuthorizationToken"]
     resources = ["*"]
   }
-
   statement {
     effect = "Allow"
     actions = [
@@ -87,24 +68,18 @@ data "aws_iam_policy_document" "ecr" {
     ]
   }
 }
-
 resource "aws_iam_policy" "ecr" {
   name        = "${aws_iam_user.cd.name}-ecr"
   description = "Allow user to manage ECR resources"
   policy      = data.aws_iam_policy_document.ecr.json
 }
-
 resource "aws_iam_user_policy_attachment" "ecr" {
   user       = aws_iam_user.cd.name
   policy_arn = aws_iam_policy.ecr.arn
 }
-
-
-
 #########################
 # Policy for EC2 access #
 #########################
-
 data "aws_iam_policy_document" "ec2" {
   statement {
     effect = "Allow"
@@ -149,19 +124,15 @@ data "aws_iam_policy_document" "ec2" {
     resources = ["*"]
   }
 }
-
 resource "aws_iam_policy" "ec2" {
   name        = "${aws_iam_user.cd.name}-ec2"
   description = "Allow user to manage EC2 resources."
   policy      = data.aws_iam_policy_document.ec2.json
 }
-
 resource "aws_iam_user_policy_attachment" "ec2" {
   user       = aws_iam_user.cd.name
   policy_arn = aws_iam_policy.ec2.arn
 }
-
-
 
 #########################
 # Policy for RDS access #
@@ -178,9 +149,7 @@ data "aws_iam_policy_document" "rds" {
       "rds:CreateDBInstance",
       "rds:DeleteDBInstance",
       "rds:ListTagsForResource",
-      "rds:ModifyDBInstance",
-      "rds:AddTagsToResource"
-
+      "rds:ModifyDBInstance"
     ]
     resources = ["*"]
   }
