fb6f86b777f1657a6adce98831e1ebf49800a0fa
Alex1.ai <alexanderemmanuel1719@gmail.com>
2025-05-27 13:51:34 +0100

    made some correction to the rds terraform configuration

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -178,7 +178,9 @@ data "aws_iam_policy_document" "rds" {
       "rds:CreateDBInstance",
       "rds:DeleteDBInstance",
       "rds:ListTagsForResource",
-      "rds:ModifyDBInstance"
+      "rds:ModifyDBInstance",
+      "rds:AddTagsToResource"
+
     ]
     resources = ["*"]
   }
