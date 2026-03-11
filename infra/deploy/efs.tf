65ed352d6e3277ac0b1c9a787d2d0c7097b1f886
Alex1-ai <alexanderemmanuel1719@gmail.com>
2026-03-11 14:06:26 +0100

    added efs

---
+++ infra/deploy/efs.tf
@@ -0,0 +1,12 @@
+#########################
+# EFS for media storage #
+#########################
+
+resource "aws_efs_file_system" "media" {
+  encrypted = true
+  tags = {
+    Name = "${local.prefix}-media"
+  }
+
+}
+
